16b506e044247cbf325e73e94525a984bf57a261
Eko Afrian <eko.afrian@harukaedu.com>
2019-04-10 10:26:22 +0700

    dragaable dan sebagian media query untu 768px

---
+++ draggable.php
@@ -0,0 +1,83 @@
+<div class="tampung1">
+	<div class="tampung2">
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/ui.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Indonesia</span>
+            <img src="/assets/image/mercu.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Mercu Buana</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/trisakti.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Trisakti</span>
+            <img src="/assets/image/itb.png" class="card-univ img-univ">
+            <span class="text-univ">Institut Teknologi Bandung</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/binus.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Bina Nusantara</span>
+            <img src="/assets/image/lspr.png" class="card-univ img-univ">
+            <span class="text-univ">LSPR</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/ukrida.png" class="card-univ img-univ">
+            <span class="text-univ">UKRIDA</span>
+            <img src="/assets/image/taruma.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Tarumanegara</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/pelita.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Pelita Harapan</span>
+            <img src="/assets/image/unpad.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Diponegoro</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/trisakti.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Trisakti</span>
+            <img src="/assets/image/itb.png" class="card-univ img-univ">
+            <span class="text-univ">Institut Teknologi Bandung</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/ukrida.png" class="card-univ img-univ">
+            <span class="text-univ">UKRIDA</span>
+            <img src="/assets/image/taruma.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Tarumanegara</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/pelita.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Pelita Harapan</span>
+            <img src="/assets/image/unpad.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Diponegoro</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/ui.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Indonesia</span>
+            <img src="/assets/image/mercu.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Mercu Buana</span>
+        </div>
+        <div class="row ml-10 tampung3">
+            <img src="/assets/image/binus.png" class="card-univ img-univ">
+            <span class="text-univ">Universitas Bina Nusantara</span>
+            <img src="/assets/image/lspr.png" class="card-univ img-univ">
+            <span class="text-univ">LSPR</span>
+        </div>
+	</div>
+</div>
+
+.tampung1 {
+	width: 100%;
+	overflow: hidden;
+	margin: 100px auto;
+}
+
+.tampung2 {
+	width: 2500px;
+	display: flex;
+	flex-direction: row;
+}
+
+.tampung3 {
+	width: 350px;
+    height: auto;
+    background: rgba(blue, 0.15);
+    overflow: auto;
+}
